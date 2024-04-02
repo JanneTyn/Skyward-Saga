a4b8ee53520fae488c2b97dd91dfb5df18f009aa
Janne <jannetyni98@gmail.com>
2024-04-02 11:15:18 +0300

    main menua päivitetty

--- Assets/Scripts/MainMenu/StartGameButton.cs
+++ Assets/Scripts/MainMenu/StartGameButton.cs
@@ -5,9 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
+    private Transform startGame;
+    private Transform options;
+    private Transform quitGame;
+    private Transform logo;
+    private Transform loadscene;
+    private Transform prologueText;
     public void PlayGame()
     {
-        SceneManager.LoadScene("Start");
-        Time.timeScale = 1;
+        startGame = transform.Find("Main Menu/Start Game");
+        options = transform.Find("Main Menu/Options");
+        quitGame = transform.Find("Main Menu/Quit to Menu");
+        logo = transform.Find("Logo");
+        loadscene = transform.Find("LoadScene");
+        prologueText = transform.Find("PrologueText");
+
+        startGame.gameObject.SetActive(false);
+        options.gameObject.SetActive(false);
+        quitGame.gameObject.SetActive(false);
+        logo.gameObject.SetActive(false);
+        prologueText.gameObject.SetActive(true);
+        loadscene.gameObject.SetActive(true);
+        
     }
 }

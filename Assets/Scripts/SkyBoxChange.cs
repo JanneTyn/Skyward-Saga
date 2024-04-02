b8a0b230e15312dd35737e424e53ca634410d865
juusos95 <56428025+juusos95@users.noreply.github.com>
2024-04-02 20:09:21 +0300

    Päivä/yö scripti

--- Assets/Scripts/SkyBoxChange.cs
+++ Assets/Scripts/SkyBoxChange.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class SkyBoxChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Material skybox;
+    [SerializeField] Color dayLight;
+    [SerializeField] Color nightLight;
+    [SerializeField] Light directionalLight;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        skybox.SetFloat("_blendValue", 0);
     }
 
     public void SkyBoxDayNightChange(bool sunActivated)
     {
+        if (!sunActivated)
+        {
+            StartCoroutine(changeOverTime(1, 1));
+        }
+        else
+            StartCoroutine(changeOverTime(0, 1));
+    }
 
+    private IEnumerator changeOverTime(float targetBlend, float duration)
+    {
+        for (float t = 0; t < duration; t += Time.deltaTime / duration)
+        {
+            float blendValue = skybox.GetFloat("_blendValue");
+
+            skybox.SetFloat("_blendValue", Mathf.Lerp(blendValue, targetBlend, t));
+
+            yield return null;
+        }
     }
 }

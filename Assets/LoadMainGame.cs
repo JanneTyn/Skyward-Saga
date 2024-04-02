82fcd97dc207f51d3309a83ddce489d95cd243b0
Janne <jannetyni98@gmail.com>
2024-04-02 12:17:50 +0300

    unohtu laittaa foliage päälle main menun jälkeen

--- Assets/LoadMainGame.cs
+++ Assets/LoadMainGame.cs
@@ -9,6 +9,7 @@ public class LoadMainGame : MonoBehaviour
     public void LoadScene()
     {
         SceneManager.LoadScene("Start");
+        SceneManager.LoadScene("Foliage", LoadSceneMode.Additive);
         Time.timeScale = 1;
     }
 }

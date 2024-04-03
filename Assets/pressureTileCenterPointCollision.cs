1974fa04bad54eb7b4a0304edfafe84ce93970d5
Janne <jannetyni98@gmail.com>
2024-04-03 12:49:25 +0300

    kiviovi vaihettu

--- Assets/pressureTileCenterPointCollision.cs
+++ Assets/pressureTileCenterPointCollision.cs
@@ -8,6 +8,7 @@ public class pressureTileCenterPointCollision : MonoBehaviour
     public GameObject vinePlatform;
     public Animator vineAnimator;
     [SerializeField] AudioSource rockonTile;
+    [SerializeField] GameObject rockdoor;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +29,7 @@ public class pressureTileCenterPointCollision : MonoBehaviour
             vineAnimator.GetComponent<Animator>().Play("lehdetAlas");
             rockonTile.time = 0.2f;
             rockonTile.Play();
+            StartCoroutine(doorDown());
             //vinePlatform.SetActive(false);
         }
     }
@@ -38,7 +40,30 @@ public class pressureTileCenterPointCollision : MonoBehaviour
         {
             Debug.Log("Kivi poissa");
             vineAnimator.GetComponent<Animator>().Play("lehdetYlos");
+            StartCoroutine(doorUp());
             //vinePlatform.SetActive(true);
+        }
+    }
+
+    IEnumerator doorDown()
+    {
+        for (float i = 0; i > -6f; i = i - 0.01f)
+        {
+            Vector3 doorlocation = rockdoor.transform.position;
+            doorlocation.y = i;
+            rockdoor.transform.position = doorlocation;
+            yield return null;
+        }      
+    }
+
+    IEnumerator doorUp()
+    {
+        for (float i = -6f; i < 0; i = i + 0.01f)
+        {
+            Vector3 doorlocation = rockdoor.transform.position;
+            doorlocation.y = i;
+            rockdoor.transform.position = doorlocation;
+            yield return null;
         }
     }
 }

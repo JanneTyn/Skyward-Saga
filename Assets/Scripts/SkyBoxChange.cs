4f8d083eef597dd5702668e53b27302c3ee170c0
juusos95 <56428025+juusos95@users.noreply.github.com>
2024-04-03 17:07:33 +0300

    Toimii?

--- Assets/Scripts/SkyBoxChange.cs
+++ Assets/Scripts/SkyBoxChange.cs
@@ -21,6 +21,9 @@ public class SkyBoxChange : MonoBehaviour
     [SerializeField] float dayDensity;
     [SerializeField] float nightDensity;
 
+    [SerializeField] Material fireflies;
+    [SerializeField] Material lightShafts;
+
     private void Start()
     {
         skybox.SetFloat("_blendValue", 0);
@@ -43,26 +46,32 @@ public class SkyBoxChange : MonoBehaviour
     private IEnumerator changeOverTime(float targetBlend, float temperature, float brightness, float shadow, Vector3 targetRot, Color fog, float density, float duration)
     {
         float blendValue = skybox.GetFloat("_blendValue");
-        /*float startTemp = dirLight.colorTemperature;
+        float startTemp = dirLight.colorTemperature;
         float startBrightness = dirLight.intensity;
         float startShadow = dirLight.shadowStrength;
-        Vector3 rot = transform.rotation.eulerAngles;*/
+        Vector3 rot = dirLight.transform.rotation.eulerAngles;
 
         Color startFog = RenderSettings.fogColor;
         float startDensity = RenderSettings.fogDensity;
+
+        Color color1 = fireflies.color;
+        Color color2 = lightShafts.color;
 
         for (float t = 0; t < duration; t += duration * Time.deltaTime)
         {
             skybox.SetFloat("_blendValue", Mathf.Lerp(blendValue, targetBlend, t));
 
-            /*dirLight.colorTemperature = Mathf.Lerp(startTemp, temperature, t);
+            dirLight.colorTemperature = Mathf.Lerp(startTemp, temperature, t);
             dirLight.intensity = Mathf.Lerp(startBrightness, brightness, t);
             dirLight.shadowStrength = Mathf.Lerp(startShadow, shadow, t);
 
-            dirLight.gameObject.transform.rotation = Quaternion.Lerp(Quaternion.Euler(rot), Quaternion.Euler(targetRot), t);*/
+            dirLight.gameObject.transform.rotation = Quaternion.Lerp(Quaternion.Euler(rot), Quaternion.Euler(targetRot), t);
 
             RenderSettings.fogColor = Color.Lerp(startFog, fog, t);
             RenderSettings.fogDensity = Mathf.Lerp(startDensity, density, t);
+
+            fireflies.color = Color.Lerp(color1, new Color(color1.r, color1.g, color1.b, targetBlend), t);
+            lightShafts.color = Color.Lerp(color2, new Color(color2.r, color2.g, color2.b, 1 - targetBlend), t);
 
             yield return null;
         }

fc7ed919082f941e4ed5529e3a73ca15fb17e752
juusos95 <juuso.nurmi95@gmail.com>
2024-04-03 13:13:02 +0300

    asioita tapahtuu

--- Assets/Scripts/SkyBoxChange.cs
+++ Assets/Scripts/SkyBoxChange.cs
@@ -16,37 +16,31 @@ public class SkyBoxChange : MonoBehaviour
     [SerializeField] Vector3 dayRot;
     [SerializeField] Vector3 nightRot;
 
-    [SerializeField] GameObject fireflyParent;
-    [SerializeField] GameObject lightShaftParent;
-    ParticleSystem[] fireflies;
-    ParticleSystem[] lightShafts;
-
     [SerializeField] Color dayFog;
     [SerializeField] Color nightFog;
+    [SerializeField] float dayDensity;
+    [SerializeField] float nightDensity;
 
     private void Start()
     {
         skybox.SetFloat("_blendValue", 0);
-
-        fireflies = fireflyParent.GetComponentsInChildren<ParticleSystem>();
-        lightShafts = lightShaftParent.GetComponentsInChildren<ParticleSystem>();
     }
 
     public void SkyBoxDayNightChange(bool sunActivated)
     {
         if (!sunActivated)
         {
-            StopCoroutine(changeOverTime(0, dayTemp, dayBrightness, dayShadowStr, dayRot, 1));
-            StartCoroutine(changeOverTime(1, nightTemp, nightBrightness, nightShadowStr, nightRot, 1));
+            StopCoroutine(changeOverTime(0, dayTemp, dayBrightness, dayShadowStr, dayRot, dayFog, dayDensity, 1));
+            StartCoroutine(changeOverTime(1, nightTemp, nightBrightness, nightShadowStr, nightRot, nightFog, nightDensity, 1));
         }
         else if (sunActivated)
         {
-            StopCoroutine(changeOverTime(1, nightTemp, nightBrightness, nightShadowStr, nightRot, 1));
-            StartCoroutine(changeOverTime(0, dayTemp, dayBrightness, dayShadowStr, dayRot, 1));
+            StopCoroutine(changeOverTime(1, nightTemp, nightBrightness, nightShadowStr, nightRot, nightFog, nightDensity, 1));
+            StartCoroutine(changeOverTime(0, dayTemp, dayBrightness, dayShadowStr, dayRot, dayFog, dayDensity, 1));
         }
     }
 
-    private IEnumerator changeOverTime(float targetBlend, float temperature, float brightness, float shadow, Vector3 targetRot, float duration)
+    private IEnumerator changeOverTime(float targetBlend, float temperature, float brightness, float shadow, Vector3 targetRot, Color fog, float density, float duration)
     {
         float blendValue = skybox.GetFloat("_blendValue");
         /*float startTemp = dirLight.colorTemperature;
@@ -54,9 +48,21 @@ public class SkyBoxChange : MonoBehaviour
         float startShadow = dirLight.shadowStrength;
         Vector3 rot = transform.rotation.eulerAngles;*/
 
+        Color startFog = RenderSettings.fogColor;
+        float startDensity = RenderSettings.fogDensity;
+
         for (float t = 0; t < duration; t += duration * Time.deltaTime)
         {
             skybox.SetFloat("_blendValue", Mathf.Lerp(blendValue, targetBlend, t));
+
+            /*dirLight.colorTemperature = Mathf.Lerp(startTemp, temperature, t);
+            dirLight.intensity = Mathf.Lerp(startBrightness, brightness, t);
+            dirLight.shadowStrength = Mathf.Lerp(startShadow, shadow, t);
+
+            dirLight.gameObject.transform.rotation = Quaternion.Lerp(Quaternion.Euler(rot), Quaternion.Euler(targetRot), t);*/
+
+            RenderSettings.fogColor = Color.Lerp(startFog, fog, t);
+            RenderSettings.fogDensity = Mathf.Lerp(startDensity, density, t);
 
             yield return null;
         }
